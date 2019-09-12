162abacebfb7eb96e23dadc9ef9e3931f8528287
Sarho Principato <sarho.principato@gmail.com>
2019-09-12 15:34:59 +0200

    Vesta SSL code update

--- vesta_ssl.sh
+++ vesta_ssl.sh
@@ -1,49 +1,37 @@
 #!/bin/bash
-# Backup original certificates
-mv /usr/local/vesta/ssl/certificate.crt /usr/local/vesta/ssl/certificate.crt.bak
-mv /usr/local/vesta/ssl/certificate.crt /usr/local/vesta/ssl/certificate.key.bak
-# Copy already verified certificates in Vesta SSL
-cp /home/admin/conf/web/ssl.my-site.com.crt /usr/local/vesta/ssl/certificate.crt
-cp /home/admin/conf/web/ssl.my-site.com.key /usr/local/vesta/ssl/certificate.key
-# Edit User, Groups and permissions
-chown root:mail /usr/local/vesta/ssl/certificate.crt
-chown root:mail /usr/local/vesta/ssl/certificate.key
-chmod 660 /usr/local/vesta/ssl/certificate.crt
-chmod 660 /usr/local/vesta/ssl/certificate.key
-# Restart services
-service vesta restart
-#service exim4 restart
-#service dovecot restart
 
-# Cron
-# TODO - Verify
-
-# This solution use simlinks
-# https://medium.com/andrewmmc-io/apply-ssl-certificate-by-lets-encrypt-to-vestacp-b2e255e93496
-
-# Backup original certificates
-mv /usr/local/vesta/ssl/certificate.crt /usr/local/vesta/ssl/certificate.crt.bak
-mv /usr/local/vesta/ssl/certificate.crt /usr/local/vesta/ssl/certificate.key.bak
-
+#Setup
 cert_src="/home/admin/conf/web/ssl.my-site.com.crt"
 key_src="/home/admin/conf/web/ssl.my-site.com.key"
 cert_dst="/usr/local/vesta/ssl/certificate.crt"
 key_dst="/usr/local/vesta/ssl/certificate.key"
 
-if ! cmp -s $cert_dst $cert_src
+# Backup original certificates
+if [ -f $cert_dst.bak_vesta_original ]
     then
-        # Copy Certificate
-        cp $cert_src $cert_dst
+        mv -f $cert_dst $cert_dst.bak
+    else
+        mv $cert_dst $cert_dst.bak_vesta_original
+fi
 
-        # Copy Keyfile
-        cp $key_src $key_dst
+if [ -f $key_dst.bak_vesta_original ]
+    then
+        mv -f $key_dst $key_dst.bak
+    else
+        mv $key_dst $key_dst.bak_vesta_original
+fi
 
-        # Change Permission
-        chown root:mail $cert_dst
-        chown root:mail $key_dst
+# Copy already verified certificates in Vesta SSL
+cp -f $cert_src $cert_dst
+cp -f $key_src $key_dst
 
-        # Restart Services
-        service vesta restart &> /dev/null
-        service exim4 restart &> /dev/null
-        service dovecot restart &> /dev/null
-    fi
+# Edit User, Groups and permissions
+chown root:mail $cert_dst
+chown root:mail $key_dst
+chmod 660 $cert_dst
+chmod 660 $key_dst
+
+# Restart services
+service vesta restart
+#service exim4 restart
+#service dovecot restart
